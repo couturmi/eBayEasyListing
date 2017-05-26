5d9331f0cd3348732a8ad69898eeb711a35f042c
Mitchell John Couturier <couturmi@mail.gvsu.edu>
2017-05-25 17:40:50 -0700

    Created Login Template

--- src/js/mainController.js
+++ src/js/mainController.js
@@ -53,5 +53,26 @@ app.controller('mainCtrl', function($scope) {
             $scope.updateShippingInfo = false;
             $scope.updateShippingText = "Change";
         }
-    }
+    };
+
+    /******************************************************
+     * Login functions
+     ******************************************************/
+    $scope.openLoginModal = function() {
+        var modal = document.getElementById('loginModal');
+        modal.style.display = "block";
+    };
+
+    $scope.closeLoginModal = function() {
+        var modal = document.getElementById('loginModal');
+        modal.style.display = "none";
+    };
+
+    // When the user clicks anywhere outside of the login modal, close it
+    window.onclick = function(event) {
+        var modal = document.getElementById('loginModal');
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
 });
