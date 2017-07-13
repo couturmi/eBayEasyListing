de7aa522a6bb577e1eab1b60754247a2fe13ecc9
Mitchell John Couturier <couturmi@mail.gvsu.edu>
2017-07-12 22:58:40 -0700

    VerifyAddItem with XML Request
    using the VerifyAddItem call with an xml request built from the listing form.
    Status: unfinished. some values in xml are hardcoded.

--- src/js/listingController.js
+++ src/js/listingController.js
@@ -12,16 +12,18 @@ app.controller('listingCtrl', ['$scope', '$http', function($scope, $http) {
     /* Initialize current listing */
     $scope.currentListing = {
         price:0.00,
-        paymentMethods:{},
-        listingType:"Fixed price",
-        duration:"Good Til' Cancelled",
+        paymentMethods:{
+            pm_payPal: true
+        },
+        listingType:"FixedPriceItem",
+        listingDuration:"Good Til' Cancelled",
         shippingLocationCountry:"United States",
         shippingLocationZIP:"48162",
         shippingLocationCityState:"Test City, Michigan"
     };
 
     /* Listing options variables */
-    $scope.listingTypeList = ["Fixed price","Auction-style"];
+    $scope.listingTypeList = ["FixedPriceItem"];
     $scope.listingDurationList = ["3 days","5 days","7 days","10 days","30 days","Good Til' Cancelled"];
     $scope.domesticShippingList = ["Flat: same cost to all buyers","Calculated: Cost varies by buyer location","Freight: large items over 150 lbs","No shipping: Local pickup only"];
 
@@ -61,38 +63,133 @@ app.controller('listingCtrl', ['$scope', '$http', function($scope, $http) {
             $scope.updateShippingInfo = false;
             $scope.updateShippingText = "Change";
         }
-    };
+    }
+
+    /******************************************
+     * XML functions
+     ******************************************/
+    var serializer = new XMLSerializer();
+
+    var createAddItemXML = function(response) {
+        /* Get XML DOM object */
+        var addItemRequest = response.responseXML;
+
+        /* Populate XML Request with input from listing form */
+        if($scope.currentListing.title)
+            addItemRequest.getElementsByTagName("Title")[0].childNodes[0].nodeValue = $scope.currentListing.title;
+        if($scope.currentListing.category)
+            //TODO: Primary Category
+        if($scope.currentListing.condition)
+            addItemRequest.getElementsByTagName("ConditionID")[0].childNodes[0].nodeValue = $scope.currentListing.condition;
+        if($scope.currentListing.desc)
+            addItemRequest.getElementsByTagName("Description")[0].childNodes[0].nodeValue = $scope.currentListing.desc;
+        if($scope.currentListing.title)
+            //TODO: Photos
+        if($scope.currentListing.listingType)
+            addItemRequest.getElementsByTagName("ListingType")[0].childNodes[0].nodeValue = $scope.currentListing.listingType;
+        if($scope.currentListing.listingDuration){
+            if($scope.currentListing.listingDuration == "Good Til' Cancelled")
+                addItemRequest.getElementsByTagName("ListingDuration")[0].childNodes[0].nodeValue = "GTC";
+            else if($scope.currentListing.listingDuration == "3 days")
+                addItemRequest.getElementsByTagName("ListingDuration")[0].childNodes[0].nodeValue = "Days_3";
+            else if($scope.currentListing.listingDuration == "5 days")
+                addItemRequest.getElementsByTagName("ListingDuration")[0].childNodes[0].nodeValue = "Days_5";
+            else if($scope.currentListing.listingDuration == "7 days")
+                addItemRequest.getElementsByTagName("ListingDuration")[0].childNodes[0].nodeValue = "Days_7";
+            else if($scope.currentListing.listingDuration == "10 days")
+                addItemRequest.getElementsByTagName("ListingDuration")[0].childNodes[0].nodeValue = "Days_10";
+            else if($scope.currentListing.listingDuration == "30 days")
+                addItemRequest.getElementsByTagName("ListingDuration")[0].childNodes[0].nodeValue = "Days_30";
+        }
+        if($scope.currentListing.price)
+            addItemRequest.getElementsByTagName("StartPrice")[0].childNodes[0].nodeValue = $scope.currentListing.price;
+        if($scope.currentListing.paymentMethods.all || $scope.currentListing.paymentMethods.pm_payPal || $scope.currentListing.paymentMethods.pm_visaMaster || $scope.currentListing.paymentMethods.pm_discover
+            || $scope.currentListing.paymentMethods.pm_amerExpr || $scope.currentListing.paymentMethods.pm_pickup) {
+            let ItemElement = addItemRequest.getElementsByTagName("Item")[0];
+            if($scope.currentListing.paymentMethods.all || $scope.currentListing.paymentMethods.pm_visaMaster){
+                let tempElement = addItemRequest.createElement("PaymentMethods");
+                let tempTextNode = addItemRequest.createTextNode("VisaMC");
+                tempElement.appendChild(tempTextNode);
+                ItemElement.appendChild(tempElement);
+            }
+            if($scope.currentListing.paymentMethods.all || $scope.currentListing.paymentMethods.pm_discover){
+                let tempElement = addItemRequest.createElement("PaymentMethods");
+                let tempTextNode = addItemRequest.createTextNode("Discover");
+                tempElement.appendChild(tempTextNode);
+                ItemElement.appendChild(tempElement);
+            }
+            if($scope.currentListing.paymentMethods.all || $scope.currentListing.paymentMethods.pm_amerExpr){
+                let tempElement = addItemRequest.createElement("PaymentMethods");
+                let tempTextNode = addItemRequest.createTextNode("AmEx");
+                tempElement.appendChild(tempTextNode);
+                ItemElement.appendChild(tempElement);
+            }
+            if($scope.currentListing.paymentMethods.all || $scope.currentListing.paymentMethods.pm_pickup){
+                let tempElement = addItemRequest.createElement("PaymentMethods");
+                let tempTextNode = addItemRequest.createTextNode("PayOnPickup");
+                tempElement.appendChild(tempTextNode);
+                ItemElement.appendChild(tempElement);
+            }
+            if($scope.currentListing.paymentMethods.all || $scope.currentListing.paymentMethods.pm_payPal){
+                let tempElement = addItemRequest.createElement("PaymentMethods");
+                let tempTextNode = addItemRequest.createTextNode("PayPal");
+                tempElement.appendChild(tempTextNode);
+                ItemElement.appendChild(tempElement);
+                let tempElement2 = addItemRequest.createElement("PayPalEmailAddress");
+                let tempTextNode2 = addItemRequest.createTextNode("mitchcout@gmail.com");
+                tempElement2.appendChild(tempTextNode2);
+                ItemElement.appendChild(tempElement2);
+            }
+        }
+        if($scope.currentListing.title)
+            //TODO: Return Options
+        if($scope.currentListing.title)
+            //TODO: Shipping Options
+
+        /* Submit Listing */
+        $scope.submitListing(serializer.serializeToString(addItemRequest));
+    }
+
+    $scope.performAddItemRequest = function() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                createAddItemXML(this);
+            }
+        };
+        xhttp.open("GET", "xmlRequests/AddItemRequest.xml", true);
+        xhttp.send();
+    }
+    $scope.performAddItemRequest();
 
     /******************************************
      * HTTP functions
      ******************************************/
-    var config = {
+    var AddItemConfig = {
             headers: {
                 'Content-Type':'text/xml',
                 'X-EBAY-API-COMPATIBILITY-LEVEL': 915,
                 'X-EBAY-API-DEV-NAME': '6387f409-1f6a-416c-88f4-4ed3f0791984',
                 'X-EBAY-API-APP-NAME': 'Mitchell-eBayEasy-SBX-f09141381-69d9c08c',
                 'X-EBAY-API-CERT-NAME': 'SBX-09141381a927-4830-414d-acb5-1173',
-                'X-EBAY-API-CALL-NAME': 'GeteBayOfficialTime',
+                'X-EBAY-API-CALL-NAME': 'VerifyAddItem',
                 'X-EBAY-API-SITEID': 0
             }
-        };
-    var xml =
-        "<?xml version='1.0' encoding='utf-8'?>" +
-        "<GeteBayOfficialTimeRequest xmlns=\"urn:ebay:apis:eBLBaseComponents\">" +
-        "  <RequesterCredentials>" +
-        "    <eBayAuthToken>AgAAAA**AQAAAA**aAAAAA**b/FjWQ**nY+sHZ2PrBmdj6wVnY+sEZ2PrA2dj6wFk4GkCpCBqQydj6x9nY+seQ**UDcEAA**AAMAAA**haPTP9tAd2O3TQv0sloQ4Rxe5S4b8mJnFSPjDTVNFC2TqGJGroTFbiyub0j2+YD+M2q+8+pvFn20bXDH7fZpcHTx7tu6WAc1FVn8YRh50su2VmVdooVhGfyDwVyDcNWwxxuAeC2b5UwhC5Q8FCtj/9lqYJb+ecj/JWnDinOrHoZSz28MRETZs/ZGGP8P1kK1tbKDgIw50HUey2TdguKcN4SOw0Fh2sx6HY5JsrVs/boBksOCuKuY97kigKVawnWsAZzAxYfe1gM7QHhLQJP8aypawwlXr5bOcGsF8M0dN7WikvbdKfeOmYMh3ar7L4J9MLt6ldAOU3PlVO5JAE1oIdo/eOKzVE/Uutq8BbmQ3n71nzsNV1dWhFsxYUiM1YAg7eG1PrZkpPDWhdku71Kv+z8o8I7m0kRTl8QChCHk24KV9K3DPO6k+uu563jR2jvwg+TxAliiCPmM+KzWKgbwwQb32ArToTD/Eti6DfGCI6LtuO92mCxC3+CEDH3ZEI06GI1zW5ZvrQuV0yjIjZ8PWLEcf0wDSmuIKteZx0frnswE8I4Kvu6L/Yv6t53j1vtBa9fGK9NQ8JDORxnC8e+sh3HnUnc/HXgYYzeYldIne3uhJbco+Kv6HexTwCEM5vTm7J6zD/zstyCC5puImiNsfl+9+J1M/Xz2eFeG+mQjxCsquKrv+rDI0F2nG78EnkohjBcJJwn3vjRXsJ0LPYQ4jYqhUMel8qd9plcDgEdk6RMYeKC5MzMmJvjrGKy1gpGW</eBayAuthToken>" +
-        "  </RequesterCredentials>" +
-        "</GeteBayOfficialTimeRequest>";
+    };
+
     var XMLParser = new DOMParser();
 
-    $scope.submitListing = function(){
-        $http.post('/ebayApiSandbox/ws/api.dll', xml, config).then( function(response){
+    $scope.submitListing = function(xmlRequest){
+        console.log(xmlRequest);
+        $http.post('/ebayApiSandbox/ws/api.dll', xmlRequest, AddItemConfig).then( function(response){
             console.log(response.data);
-            var xmlResponse = XMLParser.parseFromString(response.data,"text/xml");
-            console.log(xmlResponse.getElementsByTagName("Timestamp")[0].childNodes[0].nodeValue);
-        }, function(){
+            var ebayResponse = XMLParser.parseFromString(response.data,"text/xml");
+            console.log("========RESPONSE RESULT========");
+            console.log(ebayResponse.getElementsByTagName("Ack")[0].childNodes[0].nodeValue);
+            console.log("===============================");
+        }, function(err){
             console.log("error.");
+            console.log("Status: "+err.status+" : "+err.statusText);
         });
     };
 
